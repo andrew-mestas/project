7df3991559760414455ab01afebe78fec312b356
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-12-03 13:16:58 -0800

    incorp angular maybe for later

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 var ejsLayout = require("express-ejs-layouts");
-
+app.use(express.static(__dirname + "/static"));
 app.use(ejsLayout);
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
-	res.send("test");
+	res.render("index");
 });
 
 
-app.listen(3000);
+app.listen(3000,"0.0.0.0");
---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('UnnamedApp', []);
+
+app.controller('UnnamedCtrl', ['$scope', function($scope) {
+
+}]);
